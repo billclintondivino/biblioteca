a81e3cc0dd3533c88889c83c7ad4f099f70b0a4d
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-06-07 22:48:17 -0300

    introducing php in user login

--- login_page/login_user.php
+++ login_page/login_user.php
@@ -1,5 +1,18 @@
 <?php
 
+$usuario = $_POST['user'];
+$senha = $_POST['pass'];
 
+$usuario = stringslashes($usuario);
+$senha = stringslashes($senha);
+$usuario = mysql_real_escape_string($usuario);
+$senha = mysql_real_escape_string($senha);
+
+
+if ($row['user'] == $usuario && $row['pass'] == $senha){
+    echo "Login Successful !" .$row['user'];
+}else{
+    echo "Error in Login ! ";
+}
 
 ?>
