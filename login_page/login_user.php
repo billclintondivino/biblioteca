23ce857d2a558aa7d556df11d6e7cb9d9249a2d5
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-06-07 23:21:42 -0300

    connecting to test DB

--- login_page/login_user.php
+++ login_page/login_user.php
@@ -5,9 +5,15 @@ $senha = $_POST['pass'];
 
 $usuario = stringslashes($usuario);
 $senha = stringslashes($senha);
-$usuario = mysql_real_escape_string($usuario);
-$senha = mysql_real_escape_string($senha);
+$usuario = mysqli_real_escape_string($usuario);
+$senha = mysqli_real_escape_string($senha);
 
+mysqli_connect("localhost", "root", "");
+mysqli_select_db("login");
+}
+$result = mysqli_query("SELECT * FROM users WHERE user = '$usuario' and pass = '$senha'") or die("Falha ao consultar Banco de Dados " . mysqli_error());
+
+$row = mysqli_fetch_array($result);
 
 if ($row['user'] == $usuario && $row['pass'] == $senha){
     echo "Login Successful !" .$row['user'];
