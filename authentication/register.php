bf1ae8a61be4efd870be8bb4655dc4b57c21ebc1
Jhonatas Rodrigues da Silva Santos <jhonatasrsilvasantos@gmail.com>
2021-06-12 00:58:24 -0300

    bd use and access the login page going to the home page

---
+++ authentication/register.php
@@ -0,0 +1,74 @@
+<?php
+session_start(); //bank connection
+$db = mysqli_connect("localhost", "root", "", "authentication"); //create authentication in bank/phpMyAdmin
+
+if (isset($_POST['register_btn'])) {
+    $user = mysqli_real_escape_string($_POST['user']);
+    $email = mysqli_real_escape_string($_POST['email']);
+    $password = mysqli_real_escape_string($_POST['password']);
+    $password2 = mysqli_real_escape_string($_POST['password2']);
+
+    if ($password == $password2) {  //create user
+        $password = md5($password); //storing for security purpose
+        $sql = "INSERT INTO users(user, email, password) VALUES ('$user', '$email', '$password')";
+        mysqli_query($db, $sql);
+        $_SESSION['message'] = " login com sucesso."; //alert - successfully logged in > login successfully 
+        $_SESSION['user'] = $user;
+        header("Location:home.php"); //redirect to home page when browsing
+    } else { //alert - The two passwords are not the same
+        $_SESSION['message'] = " senhas não são idênticas!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register_Page</title>
+</head>
+
+<body>
+    <div class="header">
+        <h2>Registro do Usuario</h2>
+    </div>
+    <form method="POST" action="register.php"">
+        <table>
+            <tr>
+                <td>Usuario:</td>
+                <td>
+                    <input type=" text" name="username" class="textInput">
+        </td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td>
+                <input type="email" name="email" class="textInput">
+            </td>
+        </tr>
+        <tr>
+            <td>Senha:</td>
+            <td>
+                <input type="password" name="password" class="textInput">
+            </td>
+        </tr>
+        <!-- <tr>
+            <td>Senha Novamente:</td>
+            <td>
+                <input type="password" name="password2" class="textInput">
+            </td>
+        </tr> -->
+        <tr>
+            <td></td>
+            <td>
+                <input type="submit" name="register_btn" value="Register">
+            </td>
+        </tr>
+        </table>
+    </form>
+</body>
+
+</html>
---
+++ login/login.php
@@ -0,0 +1,23 @@
+<?php
+
+$usuario = $_POST['username'];
+$senha = $_POST['password'];
+
+// $usuario = stringslashes($usuario);
+// $senha = stringslashes($senha);
+// $usuario = mysqli_real_escape_string($usuario);
+// $senha = mysqli_real_escape_string($senha);
+
+$a = mysqli_connect("localhost", "root", "");
+mysqli_select_db($a, "login");
+
+$result = mysqli_query($a, "SELECT * FROM login  WHERE username = '$usuario' and password = '$senha'") or die("Falha ao consultar Banco de Dados " . mysqli_error());
+
+$row = mysqli_fetch_array($result);
+
+if ($row['username'] == $usuario && $row['password'] == $senha) {
+    echo "Login Successful !" . $row['username'];
+    header("Location:home.php"); // direct access to home page
+} else {
+    echo "Error in Login ! ";
+}
